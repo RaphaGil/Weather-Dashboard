208c2c24d39ab570c41b3036ec7996c824926733
raphaelagil <raphaela.gil@yahoo.com>
2024-03-13 09:38:28 +0000

    changed the layout of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,52 +13,76 @@ function displayCurrentWeather(data) {
   $("#today").empty();
   $(".weather-search").val('');
   const weatherToday = $("#today");
-  weatherToday.css({
-    "border": "gray 2px solid",
-    'background': 'white',
-    'color': 'darkgray'
-  });
+
   const weatherImg = $("<img>").attr(
-    "src",
-    `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
-  );
-  const city = data.name
+      "src",
+      `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+  ).css({
+      'width': '120px',
+      'height': '120px'
+  });
 
-  const cityChosen = $("<h1>").text(
-    `${city} ${dayjs().format("(DD/MM/YYYY)")}`
-  );
-  const dataTemp = `Temp: ${data.main.temp}°C`;
-  const dataWind = `Wind: ${data.wind.speed}KPH`;
-  const dataHum = `Humidity: ${data.main.humidity}%`;
+  const city = data.name;
+  const cityChosen = $("<h1>").text(city).css({
+      'font-weight': 'bold',
+      'color': 'rgb(252 233 106)'
+  });
 
-  const dataParagraf = $("<p>").text(dataTemp);
-  const windParagraf = $("<p>").text(dataWind);
-  const humiParagraf = $("<p>").text(dataHum);
+  const day = `${dayjs().format("dddd, DD MMM")}`;
 
-  weatherToday.append(cityChosen, dataParagraf, windParagraf, humiParagraf);
-  cityChosen.append(weatherImg);
+  const dataTemp = `${data.main.temp}°C`;
+  const dataWeather = `${data.weather[0].main}`;
+  const dataMin = `L: ${data.main.temp_min}°C`;
+  const dataMax = `H: ${data.main.temp_max}°C`;
+  const dataFeels = `Feels like: ${data.main.feels_like}°C`;
+  const dataWind = `${data.wind.speed}KPH`;
+  const dataHum = `${data.main.humidity}%`;
 
+  const dataParagraph = $("<h2>").text(dataTemp);
+  const weatherParagraph = $("<p>").text(dataWeather);
+  const minParagraph = $("<p>").text(dataMin);
+  const maxParagraph = $("<p>").text(dataMax);
+  const feelsParagraph = $("<p>").text(dataFeels);
+  const windParagraph = $("<p>").text(dataWind);
+  const humiParagraph = $("<p>").text(dataHum);
+
+  const leftDiv = $("<div>").append(cityChosen,day,dataParagraph,feelsParagraph,);
+  const midDiv = $("<div>").append(weatherImg,weatherParagraph,);
+  const rightDiv = $("<div>").append( minParagraph, maxParagraph, windParagraph, humiParagraph);
+
+  weatherToday.append(leftDiv,midDiv,rightDiv);
+
+  const searchHistory = [];
   searchHistory.push(
-  city,
-  '<br>',
-  dataTemp, 
-  weatherImg.prop('outerHTML'),
-  '<br>',  
-  dataWind, 
-  '<br>',
-  dataHum);
+      cityChosen.text(),
+      dataTemp,
+      weatherImg.prop('outerHTML'),
+      dataMax,
+      dataMin,
+      dataFeels,
+      dataWind,
+      dataHum
+  );
 
   localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
   weatherToday.css({
-    'padding':'10px'
-  })
+      'display': 'flex',
+      'justify-content':'space-around',
+      'padding': '20px',
+      'font-size': '20px',
+      'background-color': 'rgba(0, 86, 144, 0.8)',
+      'border-radius': '15px'
+  });
 }
 
+//Create and save a button for the user history 
 function appendSaveButton(cityInput) {
   const btnCreated = JSON.parse(localStorage.getItem('saveBtn')) || []; 
   const history = $(".list-group");
   const saveBtn = $("<button>");
   const userInputSave = cityInput; 
+  //New variable to compare lower and uppercase
   const userInputLower = userInputSave.toLowerCase();
   if (!btnCreated.some(btn => btn.toLowerCase() === userInputLower)) {
     saveBtn.text(userInputSave);
@@ -68,17 +92,20 @@ function appendSaveButton(cityInput) {
     localStorage.setItem('saveBtn', JSON.stringify(btnCreated));
     saveBtn.css({
       'margin-bottom': '10px',
-      'background': 'lightgray',
-      'height': '30px'
+      'height': '30px',
+      'background-color':'rgba(0, 86, 144, 0.8)',
+      'border': 'none',
+      'border-radius': '20px',
+      'width': 'auto',
+      'color': 'white'
+      
     });
 
     history.append(saveBtn);
 
     saveBtn.on("click", function (e) {
       e.preventDefault();
-      
       const cityInput = $(this).val();
-    
       searchCurrentWeather(cityInput)
         .then(function (data) {
           if (data.cod && data.cod !== 200) {
@@ -102,7 +129,7 @@ function appendSaveButton(cityInput) {
         })
     })}}
     
-
+//Delete the saveBtn from localstorage when the page is loaded 
 window.addEventListener('beforeunload', function () {
   localStorage.removeItem('saveBtn');
 })
@@ -111,36 +138,43 @@ function display5DayForecast(forecastData) {
   const forecastList = $("#forecast");
   forecastList.empty();
   const dayTextArray = [];
-
-  for (let i = 2; i < forecastData.list.length; i += 8) {
+//i += 8 because I want to show the weather every 8h as I am using 16 days forecast
+  for (let i = 2; i < forecastData.list.length; i +=8) {
     const forecastEntry = forecastData.list[i];
     const dateCreated = forecastEntry.dt_txt;
     const newDate = dateCreated.split(' ');
-    const date = dayjs(newDate[0]).format("DD/MM/YYYY")
+    const date = dayjs(newDate[0]).format("ddd")
     const weatherIcon = $("<img>").attr(
       "src",
       `http://openweathermap.org/img/w/${forecastEntry.weather[0].icon}.png`
     );
-    const temperature = `Temp: ${forecastEntry.main.temp}°C`;
-    const wind = `Wind: ${forecastEntry.wind.speed}KPH`;
-    const humidity = `Humidity: ${Math.floor(forecastEntry.main.humidity)}%`;
+    weatherIcon.css({
+      'width': '100px',
+      'height': '100px'
+    })
+    
+    const weather = `${forecastEntry.weather[0].main}`;
+    const feelsLike = `Feels like: ${forecastEntry.main.feels_like}°C`;
+    const temperature = `${forecastEntry.main.temp}°C`;
+    
 
-    const dayText = $('<p>').html('<b>' + date + '<br>' + temperature + weatherIcon[0].outerHTML + '<br>' + wind + '<br>' + humidity);
+    const dayText = $('<p>').html(weatherIcon[0].outerHTML+ '<b>' + date + '<br>' + weather+ '<br>' + temperature + '<br>' + feelsLike  );
 
+  
     const dayTextHtmlString = dayText.prop('outerHTML');
     dayTextArray.push(dayTextHtmlString);
-
+    
     const firstDay = $('<div>')
     firstDay.css({
       'display': 'flex',
-      'width': '190px',
-      'margin': '18px',
-      'height': '190px',
+      'width': '170px',
+      'margin-left': '25px',
+      'margin-bottom': '20px',
+      'height': '290px',
       'align-content': 'center',
-      'background': 'white',
-      'color': 'darkgray',
       'padding': '10px',
-      'border-radius': '5px',
+      'border-radius': '15px',
+      'background-color': 'rgba(0, 86, 144, 0.8)',
     })
     firstDay.append(dayText);
     forecastList.append(firstDay);
@@ -149,6 +183,7 @@ function display5DayForecast(forecastData) {
   }
 }
 
+//Function to display conform the btn text
 function search5DayForecast(cityInput) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${APIKey}&units=metric`;
   return fetch(apiUrl)
@@ -176,6 +211,7 @@ $("#search-button").on("click", function (e) {
   // Proceed with the API request
   searchCurrentWeather(cityInput)
     .then(function (data) {
+      console.log(data)
       if (data.cod && data.cod !== 200) {
         alert(`Input not found`);
         alert(`The city has a typo`);
